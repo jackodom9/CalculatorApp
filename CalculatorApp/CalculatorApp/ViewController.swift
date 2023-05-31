e95e388272beec795bba79d1cfcc1ee14d698c20
jackodom9 <95312160+jackodom9@users.noreply.github.com>
2023-05-31 11:59:24 -0500

    Added CalculatorApp files.

---
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Jack Edmondson Odom on 5/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
